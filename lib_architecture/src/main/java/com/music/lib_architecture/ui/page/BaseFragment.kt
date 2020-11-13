130510f07d04a27d056f23f85e2fa46bbf44f61c
zhangqinning <zhangqinning@do-global.com>
2020-11-13 14:38:31 +0800

    modify BaseFragment

--- lib_architecture/src/main/java/com/music/lib_architecture/ui/page/BaseFragment.kt
+++ lib_architecture/src/main/java/com/music/lib_architecture/ui/page/BaseFragment.kt
@@ -1,5 +1,17 @@
 package com.music.lib_architecture.ui.page
 
-open class BaseFragment:DataBindingFragment{
+import android.app.Activity
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
 
+abstract class BaseFragment : DataBindingFragment() {
+
+    fun nav() {
+    }
+
+    protected open fun toggleSoftInput() {
+        val imm =
+            requireActivity().getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS)
+    }
 }
--- lib_architecture/src/main/java/com/music/lib_architecture/ui/page/DataBindingFragment.kt
+++ lib_architecture/src/main/java/com/music/lib_architecture/ui/page/DataBindingFragment.kt
@@ -1,21 +1,38 @@
 package com.music.lib_architecture.ui.page
 
+import android.app.Activity
+import android.app.Application
+import android.content.pm.ApplicationInfo
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.Animation
+import android.widget.Toast
+import androidx.annotation.NonNull
 import androidx.core.util.keyIterator
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 abstract class DataBindingFragment : Fragment() {
 
     lateinit var mBinding: ViewDataBinding
-
-
+    private var mAnimationLoaded = false
+    private val fragmentProvider by lazy { ViewModelProvider(this) }
+    private val activityProvider by lazy { ViewModelProvider(requireActivity()) }
+    private val mApplicationViewModelProvider: ViewModelProvider by lazy {
+        ViewModelProvider(
+            requireActivity().applicationContext as BaseApplication,
+            getAppFactory(requireActivity())
+        )
+    }
 
     abstract fun initViewModel()
 
@@ -30,10 +47,19 @@ abstract class DataBindingFragment : Fragment() {
 
     abstract fun getDataBindingConfig(): DataBindingConfig
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         val dataBindingConfig = getDataBindingConfig()
 
-        val binding = DataBindingUtil.inflate<ViewDataBinding>(inflater, dataBindingConfig.layout, container, false)
+        val binding = DataBindingUtil.inflate<ViewDataBinding>(
+            inflater,
+            dataBindingConfig.layout,
+            container,
+            false
+        )
         binding.lifecycleOwner = this
         binding.setVariable(dataBindingConfig.vmVariableId, dataBindingConfig.stateViewModel)
         for (i in dataBindingConfig.bindingParams.keyIterator()) {
@@ -44,8 +70,56 @@ abstract class DataBindingFragment : Fragment() {
     }
 
     override fun onCreateAnimation(transit: Int, enter: Boolean, nextAnim: Int): Animation? {
-        GlobalScope.
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(280)
+            if (!mAnimationLoaded) {
+                mAnimationLoaded = true
+                loadInitData()
+            }
+        }
+        return super.onCreateAnimation(transit, enter, nextAnim)
     }
 
+    open fun loadInitData() {}
 
+    open fun isDebug(): Boolean {
+        return requireActivity().applicationContext.applicationInfo != null &&
+                requireActivity().applicationContext
+                    .applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE != 0
+    }
+
+    protected open fun showLongToast(text: String?) {
+        Toast.makeText(requireActivity().applicationContext, text, Toast.LENGTH_LONG).show()
+    }
+
+    protected open fun showShortToast(text: String?) {
+        Toast.makeText(requireActivity().applicationContext, text, Toast.LENGTH_SHORT).show()
+    }
+
+    protected open fun showLongToast(stringRes: Int) {
+        showLongToast(requireActivity().applicationContext.getString(stringRes))
+    }
+
+    protected open fun showShortToast(stringRes: Int) {
+        showShortToast(requireActivity().applicationContext.getString(stringRes))
+    }
+
+    fun <T : ViewModel> getFragmentViewModle(@NonNull modelClass: Class<T>) =
+        fragmentProvider.get(modelClass)
+
+    fun <T : ViewModel> getActivityViewModle(@NonNull modelClass: Class<T>) =
+        activityProvider.get(modelClass)
+
+    fun getAppFactory(activity: Activity): ViewModelProvider.Factory {
+        requireActivity()
+        return ViewModelProvider.AndroidViewModelFactory.getInstance(checkApplication(activity))
+    }
+
+    private fun checkApplication(activity: Activity): Application {
+        return activity.application
+            ?: throw IllegalStateException(
+                "Your activity/fragment is not yet attached to "
+                        + "Application. You can't request ViewModel before onCreate call."
+            )
+    }
 }
